0394e3b63cf5198dd912bdfdf685f3ca2a5e1a4d
Giovanna <giovannacasalunga@gmail.com>
2022-03-25 19:41:24 -0300

    Giovanna Casalunga

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿string frase;
+Console.WriteLine("Por favor, escreva uma frase");
+frase = Console.ReadLine()!;
+string novafrase = frase
+.Replace("r", "l"!)
+.Replace("R", "L"!);
+Console.WriteLine(novafrase);
